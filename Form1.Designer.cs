20070c81d77f9ab78266ebb67c6d13c8e5315a16
alexz429 <alexz2002429@gmail.com>
2019-09-08 02:03:01 -0400

    Panel1 and transition

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.button1 = new System.Windows.Forms.Button();
-            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.button2 = new System.Windows.Forms.Button();
+            this.flowLayoutPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // openFileDialog1
@@ -40,50 +47,120 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(275, 284);
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.Font = new System.Drawing.Font("Tw Cen MT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(109, 488);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(118, 23);
+            this.button1.Size = new System.Drawing.Size(156, 37);
             this.button1.TabIndex = 0;
             this.button1.Text = "Select Pictures";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.Button1_Click);
-            // 
-            // flowLayoutPanel1
-            // 
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(578, 26);
-            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(200, 467);
-            this.flowLayoutPanel1.TabIndex = 1;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(1123, 470);
+            this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.Font = new System.Drawing.Font("Tw Cen MT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(281, 488);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(117, 23);
+            this.button2.Size = new System.Drawing.Size(156, 37);
             this.button2.TabIndex = 2;
             this.button2.Text = "Upload Pictures";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // flowLayoutPanel
+            // 
+            this.flowLayoutPanel.BackColor = System.Drawing.Color.Transparent;
+            this.flowLayoutPanel.Location = new System.Drawing.Point(780, 30);
+            this.flowLayoutPanel.Name = "flowLayoutPanel";
+            this.flowLayoutPanel.Size = new System.Drawing.Size(469, 537);
+            this.flowLayoutPanel.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(2, -20);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(763, 545);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.White;
+            this.button3.Font = new System.Drawing.Font("Tw Cen MT", 12F);
+            this.button3.ForeColor = System.Drawing.Color.Black;
+            this.button3.Location = new System.Drawing.Point(453, 488);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(156, 37);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Clear Photos";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.AutoSize = true;
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1303, 658);
+            this.panel1.TabIndex = 5;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(443, -92);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(494, 637);
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(38)))), ((int)(((byte)(38)))));
-            this.ClientSize = new System.Drawing.Size(1329, 556);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1282, 608);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.flowLayoutPanel);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.flowLayoutPanel1);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.DoubleBuffered = true;
+            this.ForeColor = System.Drawing.Color.Brown;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Hand Spoken";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        public System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -13,12 +14,19 @@ namespace hand_spoken_frontend
 {
     public partial class Form1 : Form
     {
+        private static string BACKEND_URL = "";
         private static readonly HttpClient client = new HttpClient();
-        static List<Image> images = new List<Image>();
+        static List<KeyValuePair<string, Image>> images = new List<KeyValuePair<string,Image>>();
         public Form1()
         {
             InitializeComponent();
             InitializeOpenFileDialog();
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(button2);
+            panel1.Controls.Add(button3);
+            panel1.Controls.Add(flowLayoutPanel);
+            panel1.Controls.Add(pictureBox1);
+
         }
 
         private void InitializeOpenFileDialog()
@@ -39,14 +47,14 @@ namespace hand_spoken_frontend
             if (dr == System.Windows.Forms.DialogResult.OK)
             {
                 // Read the files
-                foreach (String file in openFileDialog1.FileNames)
+                foreach (string file in openFileDialog1.FileNames)
                 {
                     // Create a PictureBox.
                     try
                     {
                         Image loadedImage = Image.FromFile(file);
                         
-                        images.Add(loadedImage);
+                        images.Add(new KeyValuePair<string,Image>(file,loadedImage));
                     }
                     catch (Exception ex)
                     {
@@ -57,22 +65,65 @@ namespace hand_spoken_frontend
                     }
                 }
             }
-            flowLayoutPanel1.Controls.Clear();
-            foreach(Image next in images)
-            {
-                PictureBox pb = new PictureBox();
-
-                pb.Height = next.Height / 10;
-                pb.Width = next.Width / 10;
-                pb.Image = next;
-
-                flowLayoutPanel1.Controls.Add(pb);
-            }
+            refreshPhotos();
         }
 
         private void Button2_Click(object sender, EventArgs e)
         {
+            startBuf1();
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            refreshPhotos();
+        }
+
+        private void refreshPhotos()
+        {
+            flowLayoutPanel.Controls.Clear();
+            foreach (KeyValuePair<string, Image> next in images)
+            {
+                PictureBox pb = new PictureBox();
+                Label tb = new Label();
+                tb.Font = new Font("Tw Cen MT", 10);
+                tb.BackColor = Color.Transparent;
+                tb.BorderStyle = BorderStyle.None;
+                tb.ForeColor = Color.White;
+                tb.Width = 220;
+                tb.Text = next.Key.Split('\\')[next.Key.Split('\\').Length-1];//hahahahaha this is so bad
+                pb.Height = 60;
+                pb.Width = 100;
+                pb.BackgroundImage = next.Value;
+                pb.BackgroundImageLayout = ImageLayout.Stretch;
+                flowLayoutPanel.Controls.Add(pb);
+                flowLayoutPanel.Controls.Add(tb);
+                flowLayoutPanel.SetFlowBreak(tb, true);
+            }
+        }
+
+        private async void startBuf1()
+        {
+
+            var values = new Dictionary<string, string>
+            {
+            { "thing1", "hello" },
+            { "thing2", "world" }
+            };
+
+            var content = new FormUrlEncodedContent(values);
+
+            var response = await client.GetAsync("https://jsonplaceholder.typicode.com/todos/1");
+            panel1.Hide();
+            panel1.Dispose();
+            Refresh();
+            pictureBox2.Visible = true;
+            pictureBox2.Show();
+            Refresh();
+            var responseString = await response.Content.ReadAsStringAsync();
+            
             
         }
+
+
     }
 }
